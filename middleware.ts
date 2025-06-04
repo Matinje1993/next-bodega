4e0147c39cf50ce5444b21357ab195c35008a2ce
Joseph Kariuki <krafthost@gmail.com>
2025-06-04 12:03:49 +0300

    update

--- middleware.ts
+++ middleware.ts
@@ -3,3 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/:path*"],
 });
+
+export const config = {
+  matcher: [], // Don't run middleware on /api, _next, or static files
+};
