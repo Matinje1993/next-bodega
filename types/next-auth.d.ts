fc99f38592ef9d44e3272792e2874a118cceb804
Joseph Kariuki <krafthost@gmail.com>
2025-06-05 12:22:17 +0300

    update config

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,11 +2,26 @@ import NextAuth, { DefaultSession } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id?: string; // add id to user session
-        } & DefaultSession["user"];
-    }
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface User {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+  }
+
+  interface JWT {
+    id: string;
+    name?: string;
+    email?: string;
+  }
 }
 
 declare module "next-auth/jwt" {
